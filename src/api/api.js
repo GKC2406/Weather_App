08872f63d3d4758c72f59538c902e9e5a5d1e7b7
GC <100856232+GKC2406@users.noreply.github.com>
2024-06-20 01:46:04 +0530

    Add files via upload
    First Commit which includes weather app files that provides weather report.

---
+++ src/api/api.js
@@ -0,0 +1,11 @@
+const apiKey = 'YOUR_API_KEY';
+
+const getWeather = async (city) => {
+    return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+    .then((res) => res.json())
+    .then((json) => {
+        return json;
+    })
+}
+
+export default getWeather;
